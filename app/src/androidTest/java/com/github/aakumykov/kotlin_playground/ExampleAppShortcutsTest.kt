599f487f677de255fc392010a55f0ebda0d89f8a
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-18 14:56:32 +0300

    Тест на наличие ярлыков по умолчанию после установки приложения.

--- app/src/androidTest/java/com/github/aakumykov/kotlin_playground/ExampleAppShortcutsTest.kt
+++ app/src/androidTest/java/com/github/aakumykov/kotlin_playground/ExampleAppShortcutsTest.kt
@@ -17,51 +17,97 @@ import org.junit.runner.RunWith
 class ExampleAppShortcutsTest {
 
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-    private val context = InstrumentationRegistry.getInstrumentation().context
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
 
     @Test
-    fun openSystemSettings() {
-        openAppShortcuts(device, getString(R.string.settings))
+    fun openSelfApp() {
+        openAllApps()
+        openAppByName(getString(R.string.app_name))
     }
 
+
     @Test
-    fun shouldDisplayStaticShortcutsForCalendarApp() {
-//        openAllApps(device)
-//        openAppShortcuts(device, getString(R.string.app_name))
-//        verifyAppShortcuts(device, listOf(getString(R.string.action_settings)))
+    fun shouldDisplayDefaultShortcutsForSelfApp() {
+
+        openAllApps()
+
+        openAppShortcuts(getString(R.string.app_name))
+
+        verifyAppShortcuts(listOf(
+            getString(R.string.action_settings),
+            getString(R.string.gallery),
+            getString(R.string.record_video),
+            getString(R.string.selfie),
+        ))
     }
+
+
+    @Test
+    fun shouldDisplay() {
+        openAllApps()
+        openAppShortcuts(getString(R.string.calendar))
+        verifyAppShortcuts(listOf(getString(R.string.action_new_event)))
+    }
+
+
+    @Test
+    fun openSystemSettingsFromHomeScreen() {
+        pressHome();
+        openAppByName(getString(R.string.settings))
+    }
+
+
+    @Test
+    fun openSystemSettingsFromAllApps() {
+        openAllApps()
+        openAppByName(getString(R.string.settings))
+    }
+
 
     @After
     fun tearDown() {
         device.pressHome()
     }
 
+
     private fun getString(@StringRes strRes: Int): String {
         return context.getString(strRes)
     }
-}
 
-fun openAllApps(device: UiDevice) {
-    device.pressHome()
 
-    if (android.os.Build.VERSION.SDK_INT == 25) {
-        device.findObject(By.desc("Apps list"))
-            .click()
-    } else {
-        device.findObject(By.res("com.google.android.apps.nexuslauncher:id/workspace"))
-            .fling(Direction.DOWN)
+    private fun pressHome() {
+        device.pressHome()
     }
-}
-
-fun openAppShortcuts(device: UiDevice, name: String
-) {
-    device.findObject(By.desc(name)).longClick()
-}
 
 
-fun verifyAppShortcuts(device: UiDevice, shortcuts: List<String>) {
-    shortcuts.forEach { appShortcut ->
-        assertEquals(appShortcut, device.findObject(By.text(appShortcut)).text)
+    private fun openAllApps() {
+
+        device.pressHome()
+
+        if (android.os.Build.VERSION.SDK_INT == 25) {
+            openAppByName("Apps list")
+        } else {
+            device.findObject(By.res("com.google.android.apps.nexuslauncher:id/workspace"))
+                .fling(Direction.DOWN)
+        }
+    }
+
+
+    private fun openAppByName(appName: String) {
+        device.findObject(By.desc(appName)).click()
+    }
+
+
+    private fun openAppShortcuts(name: String) {
+        device.findObject(By.desc(name)).longClick()
+    }
+
+
+    private fun verifyAppShortcuts(shortcuts: List<String>) {
+        shortcuts.forEach { appShortcut ->
+            assertEquals(appShortcut, device.findObject(By.text(appShortcut)).text)
+        }
     }
 }
 
