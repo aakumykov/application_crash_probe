6e26165e46ad77d8d466bd542af1947252737c6f
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-19 13:08:54 +0300

    Revert "Начал делать отображение стутуса ярлыков по умолчанию. Не работает."
    This reverts commit dc26fa770abb3ddfb791bb9932fcfe31ae99f386.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.AbsListView.CHOICE_MODE_MULTIPLE
 import android.widget.AdapterView
-import android.widget.CheckBox
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.util.forEach
@@ -15,9 +13,15 @@ import com.github.aakumykov.android_dynamic_shortcuts_manager.DefaultShortcutsCr
 import com.github.aakumykov.android_dynamic_shortcuts_manager.dynamic_shortcut_manager.DynamicShortcutManager
 import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.ShortcutsParser
 import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.model.Shortcut
+import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.utils.RawShortcutResolver
+import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.utils.ResourceResolver
+import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.utils.ShortcutsSAXHandler
+import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.utils.ShortcutsXMLRawParser
 import com.github.aakumykov.kotlin_playground.databinding.ActivityMainBinding
 import com.github.aakumykov.kotlin_playground.extensions.getErrorMessage
+import com.github.aakumykov.kotlin_playground.extensions.showToast
 import com.google.android.material.snackbar.Snackbar
+import javax.xml.parsers.SAXParserFactory
 
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
@@ -26,8 +30,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
     private val shortcutsParser: ShortcutsParser by lazy { ShortcutsParser.getDefault(packageName, resources) }
 
     private lateinit var binding: ActivityMainBinding
-
-    private lateinit var listAdapter: ShortcutArrayAdapter
 
     private val initialShortcutList: List<Shortcut> by lazy {
         try {
@@ -38,14 +40,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         }
     }
 
-    private val shortcutList: MutableList<MutableShortcut> by lazy {
-        initialShortcutList
-            .map { MutableShortcut(it, it.enabled) }
-            .toMutableList()
-    }
+    private val shortcutList: MutableList<Shortcut> by lazy { initialShortcutList.toMutableList() }
 
-    private val checkedShortcutList: MutableList<MutableShortcut> = mutableListOf()
-    private val uncheckedShortcutList: MutableList<MutableShortcut> = mutableListOf()
+    private val checkedShortcutList: MutableList<Shortcut> = mutableListOf()
+    private val uncheckedShortcutList: MutableList<Shortcut> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,11 +52,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        listAdapter = ShortcutArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_multiple_choice,
-            shortcutList
-        )
+        val adapter = ShortcutArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, shortcutList)
 
         binding.listView.apply {
             this.adapter = adapter
@@ -76,9 +70,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         try {
             DefaultShortcutsCreator.getDefault(this).initShortcuts(R.raw.shortcuts)
             showSuccessMessage(R.string.default_shortcuts_are_crated)
-
-            listAdapter.setList(shortcutList)
-            listAdapter.notifyDataSetChanged()
         }
         catch (e: Exception) {
             showErrorMessage(e)
@@ -87,23 +78,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
 
     private fun updateShortcutsAsSelected() {
-
-        displayShortcutsCheckState()
-
         try {
-            dynamicShortcutManager.createDynamicShortcuts(checkedShortcutList.map { it.shortcut })
-            dynamicShortcutManager.removeDynamicShortcuts(uncheckedShortcutList.map { it.shortcut})
+            dynamicShortcutManager.createDynamicShortcuts(checkedShortcutList)
+            dynamicShortcutManager.removeDynamicShortcuts(uncheckedShortcutList)
             showSuccessMessage(R.string.shortcuts_are_updated)
 
         } catch (e: Exception) {
             showErrorMessage(e)
         }
-    }
-
-
-    private fun displayShortcutsCheckState() {
-        shortcutList.clear()
-
     }
 
 
@@ -122,19 +104,19 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         checkedShortcutList.clear()
 
         uncheckedShortcutList.clear()
-        uncheckedShortcutList.addAll(shortcutList.onEach { it.isEnabled = false })
+        uncheckedShortcutList.addAll(shortcutList)
 
         binding.listView.checkedItemPositions.forEach { key, value ->
             if (value) {
-                shortcutList[key].also { shortcut: MutableShortcut ->
-                    checkedShortcutList.add(shortcut.apply { isEnabled = true })
-                    uncheckedShortcutList.remove(shortcut)
+                shortcutList[key].also {
+                    checkedShortcutList.add(it)
+                    uncheckedShortcutList.remove(it)
                 }
             }
         }
 
-        Log.d(TAG, "Устан. галочки: ${checkedShortcutList.map { it.shortcut.shortcutId }.joinToString(", ")}")
-        Log.d(TAG, "Снятые галочки: ${uncheckedShortcutList.map { it.shortcut.shortcutId }.joinToString(", ")}")
+        Log.d(TAG, "Устан. галочки: ${checkedShortcutList.map { it.shortcutId }.joinToString(", ")}")
+        Log.d(TAG, "Снятые галочки: ${uncheckedShortcutList.map { it.shortcutId }.joinToString(", ")}")
     }
 
 
@@ -170,28 +152,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
 
 
-class ShortcutArrayAdapter(context: Context, itemLayoutRes: Int, initialShortcutList: MutableList<MutableShortcut>)
-    : ListViewAdapter<MutableShortcut>(context, itemLayoutRes, android.R.id.text1, initialShortcutList)
+class ShortcutArrayAdapter(context: Context, itemLayoutRes: Int, initialShortcutList: MutableList<Shortcut>)
+    : ListViewAdapter<Shortcut>(context, itemLayoutRes, android.R.id.text1, initialShortcutList)
 {
-    override fun getTitle(listItem: MutableShortcut?, itemPosition: Int): String {
-        return listItem?.shortcut?.shortcutShortLabel?.let { context.getString(it) } ?: "no-title*"
-    }
-
-    /*override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val mutableShortcut: MutableShortcut? = getItem(position)
-
-        return super.getView(position, convertView, parent).apply {
-            findViewById<CheckBox>(android.R.id.checkbox)?.also { checkBox ->
-                checkBox.isChecked = mutableShortcut?.isEnabled ?: false
-            }
-        }
-    }*/
-}
-
-
-class MutableShortcut(val shortcut: Shortcut, var isEnabled: Boolean) {
-    override fun toString(): String {
-        return MutableShortcut::class.java.simpleName + " { ${shortcut.shortcutId}, $isEnabled }"
+    override fun getTitle(listItem: Shortcut?, itemPosition: Int): String {
+        return listItem?.shortcutShortLabel?.let { context.getString(it) } ?: "no-title*"
     }
 }
