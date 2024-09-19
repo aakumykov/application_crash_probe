ddf98462aa54fc019a8cc0b474550e13d390884d
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-19 13:09:23 +0300

    Назвал переменную в ListViewAdapter "externalList", чтобы было понятно её поведение.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/ListViewAdapter.java
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/ListViewAdapter.java
@@ -16,23 +16,21 @@ import java.util.List;
 public abstract class ListViewAdapter<T> extends ArrayAdapter<T> {
 
     private final LayoutInflater inflater;
-    @LayoutRes
-    private final int layoutRes;
-    @IdRes
-    private final int titleId;
-    private final List<T> mList;
+    @LayoutRes private final int layoutRes;
+    @IdRes private final int titleId;
+    private final List<T> externalList;
 
 
     public ListViewAdapter(Context context,
                            @LayoutRes int resource,
                            @IdRes int titleId,
-                           List<T> list
+                           List<T> externalList
     ) {
-        super(context, resource, list);
+        super(context, resource, externalList);
 
         this.layoutRes = resource;
         this.titleId = titleId;
-        this.mList = list;
+        this.externalList = externalList;
 
         this.inflater = LayoutInflater.from(context);
     }
@@ -55,15 +53,15 @@ public abstract class ListViewAdapter<T> extends ArrayAdapter<T> {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        viewHolder.titleView.setText(getTitle(mList.get(position), position));
+        viewHolder.titleView.setText(getTitle(externalList.get(position), position));
 
         return convertView;
     }
 
 
     public void setList(List<T> list) {
-        mList.clear();
-        mList.addAll(list);
+        externalList.clear();
+        externalList.addAll(list);
         notifyDataSetChanged();
     }
 
