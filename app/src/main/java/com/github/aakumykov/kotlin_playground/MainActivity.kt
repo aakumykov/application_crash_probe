38164001294623919951b6c17e487e721f4176b6
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-19 12:04:00 +0300

    Работают кнопки удаления и обновления.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
@@ -26,7 +26,6 @@ import javax.xml.parsers.SAXParserFactory
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
-    private var shortcuts: List<Shortcut>? = null
     private val dynamicShortcutManager by lazy { DynamicShortcutManager(this, 4) }
     private val shortcutsParser: ShortcutsParser by lazy { ShortcutsParser.getDefault(packageName, resources) }
 
@@ -79,7 +78,24 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
 
     private fun updateShortcutsAsSelected() {
+        try {
+            dynamicShortcutManager.createDynamicShortcuts(checkedShortcutList)
+            dynamicShortcutManager.removeDynamicShortcuts(uncheckedShortcutList)
+            showSuccessMessage(R.string.shortcuts_are_updated)
 
+        } catch (e: Exception) {
+            showErrorMessage(e)
+        }
+    }
+
+
+    private fun removeAllShortcuts() {
+        try {
+            dynamicShortcutManager.removeDynamicShortcuts(initialShortcutList)
+            showSuccessMessage(R.string.all_shortcuts_are_removed)
+        } catch (e: Exception) {
+            showErrorMessage(e)
+        }
     }
 
 
@@ -104,108 +120,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
     }
 
 
-    private fun action1() {
-        val a: Int? = null
-
-        // Так не работает
-        a ?: {
-            Logger.d(TAG, "a == null")
-            showToast("a == null")
-        }
-
-        // А так работает
-//        a ?: showToast("a == null")
-
-        // И так работает
-        a?.also {
-            showToast("a != null")
-        } ?: run {
-            showToast("a == null")
-        }
-    }
-
-    private fun readShortcutsXML() {
-
-        try {
-            shortcuts = ShortcutsParser(
-                resources,
-                ShortcutsXMLRawParser(
-                    SAXParserFactory.newInstance().newSAXParser(),
-                    ShortcutsSAXHandler()
-                ),
-                RawShortcutResolver(
-                    ResourceResolver(
-                        packageName,
-                        resources
-                    )
-                )
-            )
-                .parse(R.raw.shortcuts)
-
-            Logger.d(TAG,"XML ярлыков прочитан")
-        }
-        catch (e: Exception) {
-            e.getErrorMessage().also { errorMsg ->
-                showToast(errorMsg)
-                Logger.d(TAG, errorMsg)
-            }
-        }
-    }
-
-    private fun createShortcuts() {
-
-        if (null == shortcuts)
-            readShortcutsXML()
-
-        shortcuts?.also { list ->
-            dynamicShortcutManager.removeDynamicShortcuts(list)
-
-            if (!dynamicShortcutManager.isDynamicShortcutsSupported()) {
-                showToast("Динамические ярлыки не поддерживаются!")
-                return@also
-            }
-
-            val listOf4: MutableList<Shortcut> = list.toMutableList()
-
-//            while (listOf4.size > DynamicShortcutManager.DEFAULT_MAX_SHORTCUTS_COUNT)
-//                listOf4.removeAt(Random.nextInt(0, listOf4.size))
-
-            try {
-                dynamicShortcutManager.createDynamicShortcuts(listOf4)
-                Logger.d(TAG,"Ярлыки обновлены (пересозданы?)")
-            } catch (e: Exception) {
-                e.getErrorMessage().also { errorMsg ->
-                    showToast("Ошибка: $errorMsg")
-                    Logger.e(TAG, errorMsg, e)
-                }
-            }
-
-        } ?: run {
-            showToast("Прочитайте файл ярлыков")
-        }
-    }
-
-    private fun removeAllShortcuts() {
-
-        if (null == shortcuts)
-            readShortcutsXML()
-
-        shortcuts?.also { list ->
-            dynamicShortcutManager.removeDynamicShortcuts(list)
-            Logger.d(TAG,"Ярлыки должны быть удалены")
-        } ?: run {
-            showToast("Прочитайте файл ярлыков")
-        }
-    }
-
-
-    class ShortcutArrayAdapter(context: Context, itemLayoutRes: Int, initialShortcutList: MutableList<Shortcut>)
-        : ListViewAdapter<Shortcut>(context, itemLayoutRes, android.R.id.text1, initialShortcutList)
-    {
-        override fun getTitle(listItem: Shortcut?, itemPosition: Int): String {
-            return listItem?.shortcutShortLabel?.let { context.getString(it) } ?: "no-title*"
-        }
-    }
 
     private fun string(@StringRes stringRes: Int): String = this.getString(stringRes)
 
@@ -233,5 +147,15 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
     companion object {
         val TAG: String = MainActivity::class.java.simpleName
+    }
+}
+
+
+
+class ShortcutArrayAdapter(context: Context, itemLayoutRes: Int, initialShortcutList: MutableList<Shortcut>)
+    : ListViewAdapter<Shortcut>(context, itemLayoutRes, android.R.id.text1, initialShortcutList)
+{
+    override fun getTitle(listItem: Shortcut?, itemPosition: Int): String {
+        return listItem?.shortcutShortLabel?.let { context.getString(it) } ?: "no-title*"
     }
 }
