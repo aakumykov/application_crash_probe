36afb4fffd157b67f0e178e4707ebf427350aeb6
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-23 22:46:48 +0300

    ResourceResolverInstrumentedTest

--- lib/src/androidTest/java/com/github/aakumykov/android_dynamic_shortcuts_manager/ResourceResolverInstrumentedTest.kt
+++ lib/src/androidTest/java/com/github/aakumykov/android_dynamic_shortcuts_manager/ResourceResolverInstrumentedTest.kt
@@ -13,15 +13,18 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ResourceResolverInstrumentedTest {
 
-    private lateinit var resourceResolver: ResourceResolver
+    private val resourceResolver: ResourceResolver = ResourceResolver(packageName, resources)
 
-    @Before
-    fun prepareResourceResolver() {
-        resourceResolver = ResourceResolver(packageName, resources)
-    }
 
     @Test
     fun resolve_string_resource_without_error() {
-        assertEquals("Динамические ярлыки", resourceResolver.getStringResourceByName("app_name"))
+        assertEquals(R.string.test_string,
+            resourceResolver.getStringResourceByName("test_string"))
+    }
+
+    @Test
+    fun resolve_drawable_resource_name_to_int() {
+        assertEquals(R.drawable.ic_android_black_24dp,
+            resourceResolver.getDrawableResourceByName("ic_android_black_24dp"))
     }
 }
