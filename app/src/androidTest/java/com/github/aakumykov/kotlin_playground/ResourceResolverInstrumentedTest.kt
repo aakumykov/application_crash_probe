6f0e591b377fb4f508b0ec05ed43002f7595a594
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-23 23:14:08 +0300

    Переместил ResourceResolverInstrumentedTest в модуль app, пишу ShortcutsXMLRawParserInstrumentedTests.

--- app/src/androidTest/java/com/github/aakumykov/kotlin_playground/ResourceResolverInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/aakumykov/kotlin_playground/ResourceResolverInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.github.aakumykov.android_dynamic_shortcuts_manager
+package com.github.aakumykov.kotlin_playground
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.aakumykov.android_dynamic_shortcuts_manager.common.packageName
-import com.github.aakumykov.android_dynamic_shortcuts_manager.common.resources
 import com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.utils.ResourceResolver
-import org.junit.After
+import com.github.aakumykov.kotlin_playground.utils.packageName
+import com.github.aakumykov.kotlin_playground.utils.resources
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,13 +16,15 @@ class ResourceResolverInstrumentedTest {
 
     @Test
     fun resolve_string_resource_without_error() {
-        assertEquals(R.string.test_string,
-            resourceResolver.getStringResourceByName("test_string"))
+        assertEquals(
+            com.github.aakumykov.kotlin_playground.R.string.app_name,
+            resourceResolver.getStringResourceByName("Динамические ярлыки"))
     }
 
     @Test
     fun resolve_drawable_resource_name_to_int() {
-        assertEquals(R.drawable.ic_android_black_24dp,
-            resourceResolver.getDrawableResourceByName("ic_android_black_24dp"))
+        assertEquals(
+            com.github.aakumykov.kotlin_playground.R.drawable.settings,
+            resourceResolver.getDrawableResourceByName("settings"))
     }
 }
