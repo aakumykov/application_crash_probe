1a2179d04449c593ee299d56cb7c60cdf2476c56
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-24 11:23:40 +0300

    Переопределил метод Shortcut.equals(...)

--- lib/src/main/java/com/github/aakumykov/android_dynamic_shortcuts_manager/shortcuts_parser/model/Shortcut.java
+++ lib/src/main/java/com/github/aakumykov/android_dynamic_shortcuts_manager/shortcuts_parser/model/Shortcut.java
@@ -1,5 +1,7 @@
 package com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.model;
 
+import static com.github.aakumykov.android_dynamic_shortcuts_manager.shortcuts_parser.other.ComparisonUtils.bothNullOrEquals;
+
 import android.content.Context;
 import android.content.Intent;
 
@@ -20,11 +22,17 @@ public class Shortcut {
     @StringRes @Nullable public Integer shortcutDisabledMessage;
     @Nullable public Intent shortcutIntent;
 
+
     public Shortcut(String shortcutId, boolean enabled, int icon, int shortcutShortLabel) {
         this.shortcutId = shortcutId;
         this.enabled = enabled;
         this.icon = icon;
         this.shortcutShortLabel = shortcutShortLabel;
+    }
+
+    public Shortcut(String shortcutId, boolean enabled, int icon, int shortcutShortLabel, Intent intent) {
+        this(shortcutId, enabled, icon, shortcutShortLabel);
+        this.shortcutIntent = intent;
     }
 
     public ShortcutInfoCompat toShortcutInfo(Context context) {
@@ -46,5 +54,29 @@ public class Shortcut {
             builder.setIntent(shortcutIntent);
 
         return builder.build();
+    }
+
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+
+        if (null == obj)
+            return false;
+
+        if (this == obj)
+            return true;
+
+        if (!(obj instanceof Shortcut))
+            return false;
+
+        final Shortcut s = (Shortcut) obj;
+
+        return this.shortcutId.equals(s.shortcutId) &&
+                this.enabled == s.enabled &&
+                this.icon == s.icon &&
+                this.shortcutShortLabel == s.shortcutShortLabel &&
+                bothNullOrEquals(this.shortcutLongLabel, s.shortcutLongLabel) &&
+                bothNullOrEquals(this.shortcutDisabledMessage, s.shortcutDisabledMessage) &&
+                bothNullOrEquals(this.shortcutIntent, s.shortcutIntent);
     }
 }
